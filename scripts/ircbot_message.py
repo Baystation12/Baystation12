907d47e8aab2716f7d17645f777726897240caa3
Mloc <colmohici@gmail.com>
2013-07-03 15:45:24 +0100

    Made ircbot_message.py use a password.

--- scripts/ircbot_message.py
+++ scripts/ircbot_message.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 # Two arguments, channel and message.
-# EG: "nudge.py #adminchannel ADMINHELP, people are killing me!"
+# EG: "ircbot_message.py #adminchannel ADMINHELP, people are killing me!"
 
 import sys,pickle,socket
 
@@ -11,18 +11,17 @@ def pack():
         data = sys.argv[2:] #The rest of the arguments is data
     except:
         data = "NO DATA SPECIFIED"
-    dictionary = {"ip":ip,"data":data}
+    dictionary = {"ip":ip,"data":["PASSWORD"] + data}
     pickled = pickle.dumps(dictionary)
     nudge(pickled)
 def nudge(data):
-       HOST = "IRCBOT IP HERE"
-       PORT = 45678
-       size = 1024
-       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       s.connect((HOST,PORT))
-       s.send(data)
-       s.close()
-    
+    HOST = "IRCBOT IP"
+    PORT = 45678
+    size = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+    s.send(data)
+    s.close()
+
 if __name__ == "__main__" and len(sys.argv) > 1: # If not imported and more than one argument
     pack()
-    
