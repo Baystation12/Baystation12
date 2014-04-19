TrongleCo/sealab13

Virology - NanoUI, gameplay tweaks, bugfixes
Former-commit-id: 16f787a1dae8f907c30fe0160cefe5de12023589

code/game/machinery/kitchen/smartfridge.dm
@@ -130,12 +130,25 @@
 		if(!isbroken)
 			icon_state = icon_off
 
-
 /*******************
 *   Item Adding
 ********************/
 
 /obj/machinery/smartfridge/attackby(var/obj/item/O as obj, var/mob/user as mob)
+	if(istype(O, /obj/item/weapon/screwdriver))
+		panel_open = !panel_open
+		user << "You [panel_open ? "open" : "close"] the maintenance panel."
+		overlays.Cut()
+		if(panel_open)
+			overlays += image(icon, icon_panel)
+		nanomanager.update_uis(src)
+		return
+
+	if(istype(O, /obj/item/device/multitool)||istype(O, /obj/item/weapon/wirecutters))
+		if(panel_open)
+			attack_hand(user)
+		return
+
 	if(!src.ispowered)
 		user << "<span class='notice'>\The [src] is unpowered and useless.</span>"
 		return
@@ -154,6 +167,8 @@
 			user.visible_message("<span class='notice'>[user] has added \the [O] to \the [src].", \
 								 "<span class='notice'>You add \the [O] to \the [src].")
 
+			nanomanager.update_uis(src)
+
 	else if(istype(O, /obj/item/weapon/storage/bag/plants))
 		var/obj/item/weapon/storage/bag/plants/P = O
 		var/plants_loaded = 0
@@ -171,147 +186,155 @@
 					plants_loaded++
 		if(plants_loaded)
 
-			user.visible_message("<span class='notice'>[user] loads \the [src] with \the [P].</span>", \
-								 "<span class='notice'>You load \the [src] with \the [P].</span>")
+			user.visible_message( \
+				"<span class='notice'>[user] loads \the [src] with \the [P].</span>", \
+				"<span class='notice'>You load \the [src] with \the [P].</span>")
 			if(P.contents.len > 0)
 				user << "<span class='notice'>Some items are refused.</span>"
+
+		nanomanager.update_uis(src)
 
 	else
 		user << "<span class='notice'>\The [src] smartly refuses [O].</span>"
 		return 1
 
-	updateUsrDialog()
-
 /obj/machinery/smartfridge/secure/attackby(var/obj/item/O as obj, var/mob/user as mob)
 	if (istype(O, /obj/item/weapon/card/emag))
-		src.emagged = 1
-		user << "You short out the product lock on [src]"
+		emagged = 1
+		locked = -1
+		user << "You short out the product lock on [src]."
 		return
-	else if(istype(O, /obj/item/weapon/screwdriver))
-		src.panel_open = !src.panel_open
-		user << "You [src.panel_open ? "open" : "close"] the maintenance panel."
-		src.overlays.Cut()
-		if(src.panel_open)
-			src.overlays += image(src.icon, src.icon_panel)
-		src.updateUsrDialog()
-		return
-	else if(istype(O, /obj/item/device/multitool)||istype(O, /obj/item/weapon/wirecutters))
-		if(src.panel_open)
-			attack_hand(user)
-		return
+
 	..()
-	return
 
 /obj/machinery/smartfridge/attack_paw(mob/user as mob)
-	return src.attack_hand(user)
+	return attack_hand(user)
 
 /obj/machinery/smartfridge/attack_ai(mob/user as mob)
 	return 0
 
 /obj/machinery/smartfridge/attack_hand(mob/user as mob)
-	user.set_machine(src)
-	if(src.seconds_electrified != 0)
-		if(src.shock(user, 100))
+	if(!ispowered) return
+	if(seconds_electrified != 0)
+		if(shock(user, 100))
 			return
-	interact(user)
+
+	ui_interact(user)
 
 /*******************
 *   SmartFridge Menu
 ********************/
 
-/obj/machinery/smartfridge/interact(mob/user as mob)
-	if(!src.ispowered)
-		return
+/obj/machinery/smartfridge/ui_interact(mob/user, ui_key = "main", var/datum/nanoui/ui = null)
+	user.set_machine(src)
 
-	var/dat = "<TT><b>Select an item:</b><br>"
+	var/is_secure = istype(src,/obj/machinery/smartfridge/secure)
 
-	if (contents.len == 0)
-		dat += "<font color = 'red'>No product loaded!</font>"
+	var/data[0]
+	data["contents"] = null
+	data["wires"] = null
+	data["panel_open"] = panel_open
+	data["electrified"] = seconds_electrified > 0
+	data["shoot_inventory"] = shoot_inventory
+	data["locked"] = locked
+	data["secure"] = is_secure
+
+	var/list/items[0]
+	for (var/i=1 to length(item_quants))
+		var/K = item_quants[i]
+		var/count = item_quants[K]
+		if (count > 0)
+			items.Add(list(list("display_name" = html_encode(capitalize(K)), "vend" = i, "quantity" = count)))
+
+	if (items.len > 0)
+		data["contents"] = items
+
+	var/list/vendwires = null
+	if (is_secure)
+		vendwires = list(
+			"Violet" = 1,
+			"Orange" = 2,
+			"Green" = 3)
 	else
-		for (var/O in item_quants)
-			if(item_quants[O] > 0)
-				var/N = item_quants[O]
-				dat += "<FONT color = 'blue'><B>[capitalize(O)]</B>:"
-				dat += " [N] </font>"
-				dat += "<a href='byond://?src=\ref[src];vend=[O];amount=1'>Vend</A> "
-				if(N > 5)
-					dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=5'>x5</A>)"
-					if(N > 10)
-						dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=10'>x10</A>)"
-						if(N > 25)
-							dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=25'>x25</A>)"
-				if(N > 1)
-					dat += "(<a href='?src=\ref[src];vend=[O];amount=[N]'>All</A>)"
-				dat += "<br>"
-
-		dat += "</TT>"
-	if(panel_open)
-		//One of the wires does absolutely nothing.
-		var/list/vendwires = list(
+		vendwires = list(
 			"Blue" = 1,
 			"Red" = 2,
-			"Black" = 3
-		)
-		dat += "<br><hr><br><B>Access Panel</B><br>"
-		for(var/wiredesc in vendwires)
-			var/is_uncut = src.wires & APCWireColorToFlag[vendwires[wiredesc]]
-			dat += "[wiredesc] wire: "
-			if(!is_uncut)
-				dat += "<a href='?src=\ref[src];cutwire=[vendwires[wiredesc]]'>Mend</a>"
-			else
-				dat += "<a href='?src=\ref[src];cutwire=[vendwires[wiredesc]]'>Cut</a> "
-				dat += "<a href='?src=\ref[src];pulsewire=[vendwires[wiredesc]]'>Pulse</a> "
-			dat += "<br>"
+			"Black" = 3)
 
-		dat += "<br>"
-		dat += "The orange light is [(src.seconds_electrified == 0) ? "off" : "on"].<BR>"
-		dat += "The red light is [src.shoot_inventory ? "off" : "blinking"].<BR>"
-	user << browse("<HEAD><TITLE>[src] Supplies</TITLE></HEAD><TT>[dat]</TT>", "window=smartfridge")
-	return
+	var/list/vendor_wires[0]
+	for (var/wire in vendwires)
+		var is_uncut = wires & APCWireColorToFlag[vendwires[wire]]
+		vendor_wires.Add(list(list("wire" = wire, "cut" = !is_uncut, "index" = vendwires[wire])))
+
+	if (vendor_wires.len > 0)
+		data["wires"] = vendor_wires
+
+	ui = nanomanager.try_update_ui(user, src, ui_key, ui, data)
+	if (!ui)
+		ui = new(user, src, ui_key, "smartfridge.tmpl", src.name, 400, 500)
+		ui.set_initial_data(data)
+		ui.open()
 
 /obj/machinery/smartfridge/Topic(href, href_list)
-	if(..())
-		return
-	usr.set_machine(src)
+	if (..()) return 0
 
-	if ((href_list["cutwire"]) && (src.panel_open))
-		var/twire = text2num(href_list["cutwire"])
-		if (!( istype(usr.get_active_hand(), /obj/item/weapon/wirecutters) ))
-			usr << "You need wirecutters!"
-			return
-		if (src.isWireColorCut(twire))
-			src.mend(twire)
-		else
-			src.cut(twire)
-	else if ((href_list["pulsewire"]) && (src.panel_open))
-		var/twire = text2num(href_list["pulsewire"])
-		if (!istype(usr.get_active_hand(), /obj/item/device/multitool))
-			usr << "You need a multitool!"
-			return
-		if (src.isWireColorCut(twire))
-			usr << "You can't pulse a cut wire."
-			return
-		else
-			src.pulse(twire)
-	else if (href_list["vend"])
-		var/N = href_list["vend"]
+	var/mob/user = usr
+	var/datum/nanoui/ui = nanomanager.get_open_ui(user, src, "main")
+
+	src.add_fingerprint(user)
+
+	if (href_list["close"])
+		user.unset_machine()
+		ui.close()
+		return 0
+
+	if (href_list["vend"])
+		var/index = text2num(href_list["vend"])
 		var/amount = text2num(href_list["amount"])
+		var/K = item_quants[index]
+		var/count = item_quants[K]
 
-		if(item_quants[N] <= 0) // Sanity check, there are probably ways to press the button when it shouldn't be possible.
-			return
+		// Sanity check, there are probably ways to press the button when it shouldn't be possible.
+		if(count > 0)
+			item_quants[K] = max(count - amount, 0)
 
-		item_quants[N] = max(item_quants[N] - amount, 0)
+			var/i = amount
+			for(var/obj/O in contents)
+				if (O.name == K)
+					O.loc = loc
+					i--
+					if (i <= 0)
+						return 1
 
-		var/i = amount
-		for(var/obj/O in contents)
-			if(O.name == N)
-				O.loc = src.loc
-				i--
-				if(i <= 0)
-					break
-	src.add_fingerprint(usr)
-	src.updateUsrDialog()
-	return
+		return 1
+
+	if (panel_open)
+		if (href_list["cutwire"])
+			if (!( istype(usr.get_active_hand(), /obj/item/weapon/wirecutters) ))
+				user << "You need wirecutters!"
+				return 1
+
+			var/wire_index = text2num(href_list["cutwire"])
+			if (isWireColorCut(wire_index))
+				mend(wire_index)
+			else
+				cut(wire_index)
+			return 1
+
+		if (href_list["pulsewire"])
+			if (!istype(usr.get_active_hand(), /obj/item/device/multitool))
+				usr << "You need a multitool!"
+				return 1
+
+			var/wire_index = text2num(href_list["pulsewire"])
+			if (isWireColorCut(wire_index))
+				usr << "You can't pulse a cut wire."
+				return 1
+
+			pulse(wire_index)
+			return 1
+
+	return 0
 
 /*************
 *	Hacking
@@ -332,7 +355,7 @@
 
 /obj/machinery/smartfridge/proc/mend(var/wireColor)
 	var/wireFlag = APCWireColorToFlag[wireColor]
-	var/wireIndex = APCWireColorToIndex[wireColor] //not used in this function
+	var/wireIndex = APCWireColorToIndex[wireColor]
 	src.wires |= wireFlag
 	switch(wireIndex)
 		if(WIRE_SHOCK)
@@ -347,7 +370,7 @@
 	switch(wireIndex)
 		if(WIRE_SHOCK)
 			src.seconds_electrified = 30
-		if (WIRE_SHOOTINV)
+		if(WIRE_SHOOTINV)
 			src.shoot_inventory = !src.shoot_inventory
 		if(WIRE_SCANID)
 			src.locked = -1
@@ -385,80 +408,23 @@
 	return 1
 
 /obj/machinery/smartfridge/proc/shock(mob/user, prb)
-	if(!src.ispowered)		// unpowered, no shock
-		return 0
-	if(!prob(prb))
-		return 0
+	if(!ispowered) return 0
+	if(!prob(prb)) return 0
+
 	var/datum/effect/effect/system/spark_spread/s = new /datum/effect/effect/system/spark_spread
 	s.set_up(5, 1, src)
 	s.start()
-	if (electrocute_mob(user, get_area(src), src, 0.7))
-		return 1
-	else
-		return 0
+
+	return electrocute_mob(user, get_area(src), src, 0.7)
 
 /************************
 *   Secure SmartFridges
 *************************/
 
 /obj/machinery/smartfridge/secure/Topic(href, href_list)
-	usr.set_machine(src)
-	if ((usr.contents.Find(src) || (in_range(src, usr) && istype(src.loc, /turf))))
-		if ((!src.allowed(usr)) && (!src.emagged) && (src.locked != -1) && href_list["vend"]) //For SECURE VENDING MACHINES YEAH
-			usr << "\red Access denied." //Unless emagged of course
-			return
-	..()
-	return
-
-/obj/machinery/smartfridge/secure/interact(mob/user as mob)
-
-	if(!src.ispowered)
-		return
-
-	var/dat = "<TT><b>Select an item:</b><br>"
-
-	if (contents.len == 0)
-		dat += "<font color = 'red'>No product loaded!</font>"
-	else
-		for (var/O in item_quants)
-			if(item_quants[O] > 0)
-				var/N = item_quants[O]
-				dat += "<FONT color = 'blue'><B>[capitalize(O)]</B>:"
-				dat += " [N] </font>"
-				dat += "<a href='byond://?src=\ref[src];vend=[O];amount=1'>Vend</A> "
-				if(N > 5)
-					dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=5'>x5</A>)"
-					if(N > 10)
-						dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=10'>x10</A>)"
-						if(N > 25)
-							dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=25'>x25</A>)"
-				if(N > 1)
-					dat += "(<a href='?src=\ref[src];vend=[O];amount=[N]'>All</A>)"
-				dat += "<br>"
-
-		dat += "</TT>"
-	if(panel_open)
-		var/list/vendwires = list(
-			"Violet" = 1,
-			"Orange" = 2,
-			"Green" = 3
-		)
-		dat += "<br><hr><br><B>Access Panel</B><br>"
-		for(var/wiredesc in vendwires)
-			var/is_uncut = src.wires & APCWireColorToFlag[vendwires[wiredesc]]
-			dat += "[wiredesc] wire: "
-			if(!is_uncut)
-				dat += "<a href='?src=\ref[src];cutwire=[vendwires[wiredesc]]'>Mend</a>"
-			else
-				dat += "<a href='?src=\ref[src];cutwire=[vendwires[wiredesc]]'>Cut</a> "
-				dat += "<a href='?src=\ref[src];pulsewire=[vendwires[wiredesc]]'>Pulse</a> "
-			dat += "<br>"
-
-		dat += "<br>"
-		dat += "The orange light is [(src.seconds_electrified == 0) ? "off" : "on"].<BR>"
-		//dat += "The red light is [src.shoot_inventory ? "off" : "blinking"].<BR>"
-		dat += "The green light is [src.locked == 1 ? "off" : "[src.locked == -1 ? "blinking" : "on"]"].<BR>"
-	user << browse("<HEAD><TITLE>[src] Supplies</TITLE></HEAD><TT>[dat]</TT>", "window=smartfridge")
-	return
-
-//TODO: Make smartfridges hackable. - JoeyJo0
+	if(!ispowered) return 0
+	if (usr.contents.Find(src) || (in_range(src, usr) && istype(loc, /turf)))
+		if (!allowed(usr) && !emagged && locked != -1 && href_list["vend"])
+			usr << "\red Access denied."
+			return 0
+	return ..()

Baystation12/Baystation12

Conflicts:
	.gitignore
	code/game/machinery/kitchen/smartfridge.dm
	code/game/machinery/machinery.dm
	code/modules/food/recipes_microwave.dm
	code/modules/reagents/Chemistry-Holder.dm
	code/modules/reagents/Chemistry-Reagents.dm
	code/modules/virus2/isolator.dm
	nano/js/nano_base_helpers.js

code/game/machinery/kitchen/smartfridge.dm
@@ -59,7 +59,6 @@
 		if(!isbroken)
 			icon_state = icon_off
 
-
 /*******************
 *   Item Adding
 ********************/
@@ -83,6 +82,8 @@
 			user.visible_message("<span class='notice'>[user] has added \the [O] to \the [src].", \
 								 "<span class='notice'>You add \the [O] to \the [src].")
 
+			nanomanager.update_uis(src)
+
 	else if(istype(O, /obj/item/weapon/storage/bag/plants))
 		var/obj/item/weapon/storage/bag/plants/P = O
 		var/plants_loaded = 0
@@ -100,10 +101,13 @@
 					plants_loaded++
 		if(plants_loaded)
 
-			user.visible_message("<span class='notice'>[user] loads \the [src] with \the [P].</span>", \
-								 "<span class='notice'>You load \the [src] with \the [P].</span>")
+			user.visible_message( \
+				"<span class='notice'>[user] loads \the [src] with \the [P].</span>", \
+				"<span class='notice'>You load \the [src] with \the [P].</span>")
 			if(P.contents.len > 0)
 				user << "<span class='notice'>Some items are refused.</span>"
+
+		nanomanager.update_uis(src)
 
 	else
 		user << "<span class='notice'>\The [src] smartly refuses [O].</span>"
@@ -111,70 +115,74 @@
 
 	updateUsrDialog()
 
+	..()
+
 /obj/machinery/smartfridge/attack_paw(mob/user as mob)
-	return src.attack_hand(user)
+	return attack_hand(user)
 
 /obj/machinery/smartfridge/attack_ai(mob/user as mob)
 	return 0
 
-/obj/machinery/smartfridge/attack_hand(mob/user as mob)
-	user.set_machine(src)
-	interact(user)
+	ui_interact(user)
 
 /*******************
 *   SmartFridge Menu
 ********************/
 
-/obj/machinery/smartfridge/interact(mob/user as mob)
-	if(!src.ispowered)
-		return
+/obj/machinery/smartfridge/ui_interact(mob/user, ui_key = "main", var/datum/nanoui/ui = null)
+	user.set_machine(src)
 
-	var/dat = "<TT><b>Select an item:</b><br>"
+	var/data[0]
+	data["contents"] = null
 
-	if (contents.len == 0)
-		dat += "<font color = 'red'>No product loaded!</font>"
-	else
-		for (var/O in item_quants)
-			if(item_quants[O] > 0)
-				var/N = item_quants[O]
-				dat += "<FONT color = 'blue'><B>[capitalize(O)]</B>:"
-				dat += " [N] </font>"
-				dat += "<a href='byond://?src=\ref[src];vend=[O];amount=1'>Vend</A> "
-				if(N > 5)
-					dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=5'>x5</A>)"
-					if(N > 10)
-						dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=10'>x10</A>)"
-						if(N > 25)
-							dat += "(<a href='byond://?src=\ref[src];vend=[O];amount=25'>x25</A>)"
-				if(N > 1)
-					dat += "(<a href='?src=\ref[src];vend=[O];amount=[N]'>All</A>)"
-				dat += "<br>"
+	var/list/items[0]
+	for (var/i=1 to length(item_quants))
+		var/K = item_quants[i]
+		var/count = item_quants[K]
+		if (count > 0)
+			items.Add(list(list("display_name" = html_encode(capitalize(K)), "vend" = i, "quantity" = count)))
 
-		dat += "</TT>"
-	user << browse("<HEAD><TITLE>[src] Supplies</TITLE></HEAD><TT>[dat]</TT>", "window=smartfridge")
-	onclose(user, "smartfridge")
-	return
+	if (items.len > 0)
+		data["contents"] = items
+
+
+	ui = nanomanager.try_update_ui(user, src, ui_key, ui, data)
+	if (!ui)
+		ui = new(user, src, ui_key, "smartfridge.tmpl", src.name, 400, 500)
+		ui.set_initial_data(data)
+		ui.open()
 
 /obj/machinery/smartfridge/Topic(href, href_list)
-	if(..())
-		return
-	usr.set_machine(src)
+	if (..()) return 0
 
-	var/N = href_list["vend"]
-	var/amount = text2num(href_list["amount"])
+	var/mob/user = usr
+	var/datum/nanoui/ui = nanomanager.get_open_ui(user, src, "main")
 
-	if(item_quants[N] <= 0) // Sanity check, there are probably ways to press the button when it shouldn't be possible.
-		return
+	src.add_fingerprint(user)
 
-	item_quants[N] = max(item_quants[N] - amount, 0)
+	if (href_list["close"])
+		user.unset_machine()
+		ui.close()
+		return 0
 
-	var/i = amount
-	for(var/obj/O in contents)
-		if(O.name == N)
-			O.loc = src.loc
-			i--
-			if(i <= 0)
-				break
+	if (href_list["vend"])
+		var/index = text2num(href_list["vend"])
+		var/amount = text2num(href_list["amount"])
+		var/K = item_quants[index]
+		var/count = item_quants[K]
 
-	src.updateUsrDialog()
-	return
+		// Sanity check, there are probably ways to press the button when it shouldn't be possible.
+		if(count > 0)
+			item_quants[K] = max(count - amount, 0)
+
+			var/i = amount
+			for(var/obj/O in contents)
+				if (O.name == K)
+					O.loc = loc
+					i--
+					if (i <= 0)
+						return 1
+
+		return 1
+
+	return 0