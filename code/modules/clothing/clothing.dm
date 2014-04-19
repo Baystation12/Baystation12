TrongleCo/sealab13

Undershirts and rolling down jumpsuits!
Former-commit-id: 7b7f3bcfc0a5fbe137281b20db939c438eb35b9f

code/modules/clothing/clothing.dm
@@ -241,6 +241,9 @@ BLIND     // can't see anything
 		*/
 	var/obj/item/clothing/tie/hastie = null
 	var/displays_id = 1
+	var/rolled_down = 0
+	var/basecolor
+
 
 /obj/item/clothing/under/attackby(obj/item/I, mob/user)
 	if(!hastie && istype(I, /obj/item/clothing/tie))
@@ -306,6 +309,22 @@ BLIND     // can't see anything
 		if(3)
 			usr << "Your suit will now report your vital lifesigns as well as your coordinate position."
 	..()
+
+/obj/item/clothing/under/verb/rollsuit()
+	set name = "Roll Down Jumpsuit"
+	set category = "Object"
+	set src in usr
+	if(!istype(usr, /mob/living)) return
+	if(usr.stat) return
+
+	if(copytext(item_color,-2) != "_d")
+		basecolor = item_color
+	usr << "DEBUG:[basecolor]"
+	if(basecolor + "_d_s" in icon_states('icons/mob/uniform.dmi'))
+		item_color = item_color == "[basecolor]" ? "[basecolor]_d" : "[basecolor]"
+		usr.update_inv_w_uniform()
+	else
+		usr << "<span class='notice'>You cannot roll down the uniform!</span>"
 
 /obj/item/clothing/under/verb/removetie()
 	set name = "Remove Accessory"

Baystation12/Baystation12

Conflicts:
	code/modules/client/preferences.dm
	code/modules/client/preferences_savefile.dm
	icons/mob/human.dmi
	icons/mob/uniform.dmi

code/modules/clothing/clothing.dm
@@ -249,6 +249,9 @@ BLIND     // can't see anything
 		*/
 	var/obj/item/clothing/tie/hastie = null
 	var/displays_id = 1
+	var/rolled_down = 0
+	var/basecolor
+
 
 /obj/item/clothing/under/attackby(obj/item/I, mob/user)
 	if(!hastie && istype(I, /obj/item/clothing/tie))
@@ -312,6 +315,22 @@ BLIND     // can't see anything
 		if(3)
 			usr << "Your suit will now report your vital lifesigns as well as your coordinate position."
 	..()
+
+/obj/item/clothing/under/verb/rollsuit()
+	set name = "Roll Down Jumpsuit"
+	set category = "Object"
+	set src in usr
+	if(!istype(usr, /mob/living)) return
+	if(usr.stat) return
+
+	if(copytext(_color,-2) != "_d")
+		basecolor = _color
+	usr << "DEBUG:[basecolor]"
+	if(basecolor + "_d_s" in icon_states('icons/mob/uniform.dmi'))
+		_color = _color == "[basecolor]" ? "[basecolor]_d" : "[basecolor]"
+		usr.update_inv_w_uniform()
+	else
+		usr << "<span class='notice'>You cannot roll down the uniform!</span>"
 
 /obj/item/clothing/under/verb/removetie()
 	set name = "Remove Accessory"