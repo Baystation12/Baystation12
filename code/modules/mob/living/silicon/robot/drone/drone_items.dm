TrongleCo/sealab13

Adds drones, drone fabrication, commit options. This is the squashed version of the original 22 commit pull, so I'm summarizing.
Former-commit-id: 1a24e08f365e2ae6fae745a0b106446adf7f1c3d

code/modules/mob/living/silicon/robot/drone/drone_items.dm
@@ -1,0 +1,251 @@
+//Simple borg hand.
+//Limited use.
+/obj/item/weapon/gripper
+	name = "magnetic gripper"
+	desc = "A simple grasping tool for synthetic assets."
+	icon = 'icons/obj/device.dmi'
+	icon_state = "gripper"
+
+	//Has a list of items that it can hold.
+	var/list/can_hold = list(
+		/obj/item/weapon/cell,
+		/obj/item/weapon/firealarm_electronics,
+		/obj/item/weapon/airalarm_electronics,
+		/obj/item/weapon/airlock_electronics,
+		/obj/item/weapon/module/power_control,
+		/obj/item/weapon/stock_parts,
+		/obj/item/light_fixture_frame,
+		/obj/item/apc_frame,
+		/obj/item/alarm_frame,
+		/obj/item/firealarm_frame,
+		/obj/item/weapon/table_parts,
+		/obj/item/weapon/rack_parts,
+		/obj/item/weapon/camera_assembly,
+		)
+
+	//Item currently being held.
+	var/obj/item/wrapped = null
+
+/obj/item/weapon/gripper/attack_self(mob/user as mob)
+	if(wrapped)
+		wrapped.attack_self(user)
+
+/obj/item/weapon/gripper/verb/drop_item()
+
+	set name = "Drop Item"
+	set desc = "Release an item from your magnetic gripper."
+	set category = "Drone"
+
+	if(!wrapped)
+		//There's some weirdness with items being lost inside the arm. Trying to fix all cases. ~Z
+		for(var/obj/item/thing in src.contents)
+			thing.loc = get_turf(src)
+		return
+
+	if(wrapped.loc != src)
+		wrapped = null
+		return
+
+	src.loc << "\red You drop \the [wrapped]."
+	wrapped.loc = get_turf(src)
+	wrapped = null
+	//update_icon()
+
+/obj/item/weapon/gripper/afterattack(atom/target, mob/user as mob)
+
+	if(!target) //Target is invalid.
+		return
+
+	//There's some weirdness with items being lost inside the arm. Trying to fix all cases. ~Z
+	if(!wrapped)
+		for(var/obj/item/thing in src.contents)
+			wrapped = thing
+			break
+
+	if(wrapped) //Already have an item.
+
+		wrapped.loc = user
+		//Pass the attack on to the target.
+		target.attackby(wrapped,user)
+
+		if(wrapped && src && wrapped.loc == user)
+			wrapped.loc = src
+
+		//Sanity/item use checks.
+
+		if(!wrapped || !user)
+			return
+
+		if(wrapped.loc != src.loc)
+			wrapped = null
+			return
+
+	if(istype(target,/obj/item)) //Check that we're not pocketing a mob.
+
+		//...and that the item is not in a container.
+		if(!isturf(target.loc))
+			return
+
+		var/obj/item/I = target
+
+		//Check if the item is blacklisted.
+		var/grab = 0
+		for(var/typepath in can_hold)
+			if(istype(I,typepath))
+				grab = 1
+				break
+
+		//We can grab the item, finally.
+		if(grab)
+			user << "You collect \the [I]."
+			I.loc = src
+			wrapped = I
+			return
+		else
+			user << "\red Your gripper cannot hold \the [target]."
+
+//TODO: Matter decompiler.
+/obj/item/weapon/matter_decompiler
+
+	name = "matter decompiler"
+	desc = "Eating trash, bits of glass, or other debris will replenish your stores."
+	icon = 'icons/obj/device.dmi'
+	icon_state = "decompiler"
+
+	//Metal, glass, wood, plastic.
+	var/list/stored_comms = list(
+		"metal" = 0,
+		"glass" = 0,
+		"wood" = 0,
+		"plastic" = 0
+		)
+
+/obj/item/weapon/matter_decompiler/afterattack(atom/target, mob/user as mob)
+
+	//We only want to deal with using this on turfs. Specific items aren't important.
+	var/turf/T = get_turf(target)
+	if(!istype(T))
+		return
+
+	//Used to give the right message.
+	var/grabbed_something = 0
+
+	for(var/obj/item/W in T)
+		//Different classes of items give different commodities.
+		if(istype(W,/obj/item/trash))
+			stored_comms["metal"]++
+			stored_comms["plastic"]++
+		else if(istype(W,/obj/effect/decal/cleanable/blood/gibs/robot))
+			stored_comms["metal"]++
+			stored_comms["glass"]++
+		else if(istype(W,/obj/item/ammo_casing))
+			stored_comms["metal"]++
+		else if(istype(W,/obj/item/weapon/shard/shrapnel))
+			stored_comms["metal"]++
+		else if(istype(W,/obj/item/weapon/shard))
+			stored_comms["glass"]++
+		else if(istype(W,/obj/item/weapon/reagent_containers/food/snacks/grown))
+			stored_comms["wood"]++
+		else
+			continue
+
+		del(W)
+		grabbed_something = 1
+
+	if(grabbed_something)
+		user << "\blue You deploy your decompiler and clear out the contents of \the [T]."
+	else
+		user << "\red Nothing on \the [T] is useful to you."
+	return
+
+//PRETTIER TOOL LIST.
+/mob/living/silicon/robot/drone/installed_modules()
+
+	if(weapon_lock)
+		src << "\red Weapon lock active, unable to use modules! Count:[weaponlock_time]"
+		return
+
+	if(!module)
+		module = new /obj/item/weapon/robot_module/drone(src)
+
+	var/dat = "<HEAD><TITLE>Drone modules</TITLE><META HTTP-EQUIV='Refresh' CONTENT='10'></HEAD><BODY>\n"
+	dat += {"<A HREF='?src=\ref[src];mach_close=robotmod'>Close</A>
+	<BR>
+	<BR>
+	<B>Activated Modules</B>
+	<BR>
+	Module 1: [module_state_1 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_1]>[module_state_1]<A>" : "No Module"]<BR>
+	Module 2: [module_state_2 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_2]>[module_state_2]<A>" : "No Module"]<BR>
+	Module 3: [module_state_3 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_3]>[module_state_3]<A>" : "No Module"]<BR>
+	<BR>
+	<B>Installed Modules</B><BR><BR>"}
+
+
+	var/tools = "<B>Tools and devices</B><BR>"
+	var/resources = "<BR><B>Resources</B><BR>"
+
+	for (var/O in module.modules)
+
+		var/module_string = ""
+
+		if (!O)
+			module_string += text("<B>Resource depleted</B><BR>")
+		else if(activated(O))
+			module_string += text("[O]: <B>Activated</B><BR>")
+		else
+			module_string += text("[O]: <A HREF=?src=\ref[src];act=\ref[O]>Activate</A><BR>")
+
+		if((istype(O,/obj/item/weapon) || istype(O,/obj/item/device)) && !(istype(O,/obj/item/weapon/cable_coil)))
+			tools += module_string
+		else
+			resources += module_string
+
+	dat += tools
+
+	if (emagged)
+		if (module.emag)
+			dat += text("<B>Resource depleted</B><BR>")
+		else if(activated(module.emag))
+			dat += text("[module.emag]: <B>Activated</B><BR>")
+		else
+			dat += text("[module.emag]: <A HREF=?src=\ref[src];act=\ref[module.emag]>Activate</A><BR>")
+
+	dat += resources
+
+	src << browse(dat, "window=robotmod&can_close=0")
+
+//Putting the decompiler here to avoid doing list checks every tick.
+/mob/living/silicon/robot/drone/use_power()
+
+	..()
+	if(!src.has_power || !decompiler)
+		return
+
+	//The decompiler replenishes drone stores from hoovered-up junk each tick.
+	for(var/type in decompiler.stored_comms)
+		if(decompiler.stored_comms[type] > 0)
+			var/obj/item/stack/sheet/stack
+			switch(type)
+				if("metal")
+					if(!stack_metal)
+						stack_metal = new(src.module)
+						stack_metal.amount = 1
+					stack = stack_metal
+				if("glass")
+					if(!stack_glass)
+						stack_glass = new(src.module)
+						stack_glass.amount = 1
+					stack = stack_glass
+				if("wood")
+					if(!stack_wood)
+						stack_wood = new(src.module)
+						stack_wood.amount = 1
+					stack = stack_wood
+				if("plastic")
+					if(!stack_plastic)
+						stack_plastic = new(src.module)
+						stack_plastic.amount = 1
+					stack = stack_plastic
+
+			stack.amount++
+			decompiler.stored_comms[type]--;

code/modules/mob/living/silicon/robot/robot_modules.dm
@@ -266,3 +266,73 @@
 		src.modules += new /obj/item/weapon/wrench(src) //Is a combat android really going to be stopped by a chair?
 		src.emag = new /obj/item/weapon/gun/energy/lasercannon/cyborg(src)
 		return
+
+
+/obj/item/weapon/robot_module/drone
+	name = "drone module"
+
+
+	New()
+		//TODO: Replace with shittier flashlight and work out why we can't remove the flash. ~Z
+		..()
+		src.modules += new /obj/item/weapon/weldingtool(src)
+		src.modules += new /obj/item/weapon/screwdriver(src)
+		src.modules += new /obj/item/weapon/wrench(src)
+		src.modules += new /obj/item/weapon/crowbar(src)
+		src.modules += new /obj/item/weapon/wirecutters(src)
+		src.modules += new /obj/item/device/multitool(src)
+		src.modules += new /obj/item/device/lightreplacer(src)
+		src.modules += new /obj/item/weapon/reagent_containers/spray/cleaner(src)
+		src.modules += new /obj/item/weapon/gripper(src)
+		src.modules += new /obj/item/weapon/matter_decompiler(src)
+
+		src.emag = new /obj/item/weapon/card/emag(src)
+		src.emag.name = "Cryptographic Sequencer"
+
+
+		var/list/stacktypes = list(
+			/obj/item/stack/rods = 10,
+			/obj/item/stack/tile/plasteel = 10,
+			/obj/item/stack/sheet/metal/cyborg = 10,
+			/obj/item/stack/sheet/wood/cyborg = 1,
+			/obj/item/weapon/cable_coil = 30,
+			/obj/item/stack/sheet/glass/cyborg = 10,
+			/obj/item/stack/sheet/mineral/plastic/cyborg = 1
+			)
+
+		for(var/T in stacktypes)
+			var/obj/item/stack/sheet/W = new T(src)
+			W.amount = stacktypes[T]
+			src.modules += W
+
+		return
+
+/obj/item/weapon/robot_module/drone/respawn_consumable(var/mob/living/silicon/robot/R)
+	var/obj/item/weapon/reagent_containers/spray/cleaner/C = locate() in src.modules
+	C.reagents.add_reagent("cleaner", 10)
+
+	var/list/stacks = list (
+		/obj/item/stack/sheet/metal,
+		/obj/item/weapon/cable_coil,
+		/obj/item/stack/sheet/glass/cyborg,
+		/obj/item/stack/rods,
+		/obj/item/stack/tile/plasteel
+	)
+
+	for(var/T in stacks)
+		var/O = locate(T) in src.modules
+		var/obj/item/stack/sheet/S = O
+
+		if(!S)
+			src.modules -= null
+			S = new T(src)
+			src.modules += S
+			S.amount = 0
+
+		if(istype(S) && S.amount < 15)
+			S.amount++
+
+	var/obj/item/device/lightreplacer/LR = locate() in src.modules
+	LR.Charge(R)
+
+	return

Baystation12/Baystation12

Conflicts:
	baystation12.dme
	code/controllers/configuration.dm
	code/game/objects/items/weapons/AI_modules.dm
	code/modules/mob/living/living.dm
	code/modules/mob/living/silicon/robot/examine.dm
	code/modules/mob/living/silicon/robot/life.dm
	code/modules/mob/living/silicon/robot/robot.dm
	code/modules/mob/living/silicon/robot/robot_modules.dm
	code/modules/recycling/disposal.dm
	icons/mob/robots.dmi
	icons/obj/device.dmi

code/modules/mob/living/silicon/robot/drone/drone_items.dm
@@ -1,0 +1,251 @@
+//Simple borg hand.
+//Limited use.
+/obj/item/weapon/gripper
+	name = "magnetic gripper"
+	desc = "A simple grasping tool for synthetic assets."
+	icon = 'icons/obj/device.dmi'
+	icon_state = "gripper"
+
+	//Has a list of items that it can hold.
+	var/list/can_hold = list(
+		/obj/item/weapon/cell,
+		/obj/item/weapon/firealarm_electronics,
+		/obj/item/weapon/airalarm_electronics,
+		/obj/item/weapon/airlock_electronics,
+		/obj/item/weapon/module/power_control,
+		/obj/item/weapon/stock_parts,
+		/obj/item/light_fixture_frame,
+		/obj/item/apc_frame,
+		/obj/item/alarm_frame,
+		/obj/item/firealarm_frame,
+		/obj/item/weapon/table_parts,
+		/obj/item/weapon/rack_parts,
+		/obj/item/weapon/camera_assembly,
+		)
+
+	//Item currently being held.
+	var/obj/item/wrapped = null
+
+/obj/item/weapon/gripper/attack_self(mob/user as mob)
+	if(wrapped)
+		wrapped.attack_self(user)
+
+/obj/item/weapon/gripper/verb/drop_item()
+
+	set name = "Drop Item"
+	set desc = "Release an item from your magnetic gripper."
+	set category = "Drone"
+
+	if(!wrapped)
+		//There's some weirdness with items being lost inside the arm. Trying to fix all cases. ~Z
+		for(var/obj/item/thing in src.contents)
+			thing.loc = get_turf(src)
+		return
+
+	if(wrapped.loc != src)
+		wrapped = null
+		return
+
+	src.loc << "\red You drop \the [wrapped]."
+	wrapped.loc = get_turf(src)
+	wrapped = null
+	//update_icon()
+
+/obj/item/weapon/gripper/afterattack(atom/target, mob/user as mob)
+
+	if(!target) //Target is invalid.
+		return
+
+	//There's some weirdness with items being lost inside the arm. Trying to fix all cases. ~Z
+	if(!wrapped)
+		for(var/obj/item/thing in src.contents)
+			wrapped = thing
+			break
+
+	if(wrapped) //Already have an item.
+
+		wrapped.loc = user
+		//Pass the attack on to the target.
+		target.attackby(wrapped,user)
+
+		if(wrapped && src && wrapped.loc == user)
+			wrapped.loc = src
+
+		//Sanity/item use checks.
+
+		if(!wrapped || !user)
+			return
+
+		if(wrapped.loc != src.loc)
+			wrapped = null
+			return
+
+	if(istype(target,/obj/item)) //Check that we're not pocketing a mob.
+
+		//...and that the item is not in a container.
+		if(!isturf(target.loc))
+			return
+
+		var/obj/item/I = target
+
+		//Check if the item is blacklisted.
+		var/grab = 0
+		for(var/typepath in can_hold)
+			if(istype(I,typepath))
+				grab = 1
+				break
+
+		//We can grab the item, finally.
+		if(grab)
+			user << "You collect \the [I]."
+			I.loc = src
+			wrapped = I
+			return
+		else
+			user << "\red Your gripper cannot hold \the [target]."
+
+//TODO: Matter decompiler.
+/obj/item/weapon/matter_decompiler
+
+	name = "matter decompiler"
+	desc = "Eating trash, bits of glass, or other debris will replenish your stores."
+	icon = 'icons/obj/device.dmi'
+	icon_state = "decompiler"
+
+	//Metal, glass, wood, plastic.
+	var/list/stored_comms = list(
+		"metal" = 0,
+		"glass" = 0,
+		"wood" = 0,
+		"plastic" = 0
+		)
+
+/obj/item/weapon/matter_decompiler/afterattack(atom/target, mob/user as mob)
+
+	//We only want to deal with using this on turfs. Specific items aren't important.
+	var/turf/T = get_turf(target)
+	if(!istype(T))
+		return
+
+	//Used to give the right message.
+	var/grabbed_something = 0
+
+	for(var/obj/item/W in T)
+		//Different classes of items give different commodities.
+		if(istype(W,/obj/item/trash))
+			stored_comms["metal"]++
+			stored_comms["plastic"]++
+		else if(istype(W,/obj/effect/decal/cleanable/robot_debris))
+			stored_comms["metal"]++
+			stored_comms["glass"]++
+		else if(istype(W,/obj/item/ammo_casing))
+			stored_comms["metal"]++
+		else if(istype(W,/obj/item/weapon/shard/shrapnel))
+			stored_comms["metal"]++
+		else if(istype(W,/obj/item/weapon/shard))
+			stored_comms["glass"]++
+		else if(istype(W,/obj/item/weapon/reagent_containers/food/snacks/grown))
+			stored_comms["wood"]++
+		else
+			continue
+
+		del(W)
+		grabbed_something = 1
+
+	if(grabbed_something)
+		user << "\blue You deploy your decompiler and clear out the contents of \the [T]."
+	else
+		user << "\red Nothing on \the [T] is useful to you."
+	return
+
+//PRETTIER TOOL LIST.
+/mob/living/silicon/robot/drone/installed_modules()
+
+	if(weapon_lock)
+		src << "\red Weapon lock active, unable to use modules! Count:[weaponlock_time]"
+		return
+
+	if(!module)
+		module = new /obj/item/weapon/robot_module/drone(src)
+
+	var/dat = "<HEAD><TITLE>Drone modules</TITLE><META HTTP-EQUIV='Refresh' CONTENT='10'></HEAD><BODY>\n"
+	dat += {"<A HREF='?src=\ref[src];mach_close=robotmod'>Close</A>
+	<BR>
+	<BR>
+	<B>Activated Modules</B>
+	<BR>
+	Module 1: [module_state_1 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_1]>[module_state_1]<A>" : "No Module"]<BR>
+	Module 2: [module_state_2 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_2]>[module_state_2]<A>" : "No Module"]<BR>
+	Module 3: [module_state_3 ? "<A HREF=?src=\ref[src];mod=\ref[module_state_3]>[module_state_3]<A>" : "No Module"]<BR>
+	<BR>
+	<B>Installed Modules</B><BR><BR>"}
+
+
+	var/tools = "<B>Tools and devices</B><BR>"
+	var/resources = "<BR><B>Resources</B><BR>"
+
+	for (var/O in module.modules)
+
+		var/module_string = ""
+
+		if (!O)
+			module_string += text("<B>Resource depleted</B><BR>")
+		else if(activated(O))
+			module_string += text("[O]: <B>Activated</B><BR>")
+		else
+			module_string += text("[O]: <A HREF=?src=\ref[src];act=\ref[O]>Activate</A><BR>")
+
+		if((istype(O,/obj/item/weapon) || istype(O,/obj/item/device)) && !(istype(O,/obj/item/stack/cable_coil)))
+			tools += module_string
+		else
+			resources += module_string
+
+	dat += tools
+
+	if (emagged)
+		if (module.emag)
+			dat += text("<B>Resource depleted</B><BR>")
+		else if(activated(module.emag))
+			dat += text("[module.emag]: <B>Activated</B><BR>")
+		else
+			dat += text("[module.emag]: <A HREF=?src=\ref[src];act=\ref[module.emag]>Activate</A><BR>")
+
+	dat += resources
+
+	src << browse(dat, "window=robotmod&can_close=0")
+
+//Putting the decompiler here to avoid doing list checks every tick.
+/mob/living/silicon/robot/drone/use_power()
+
+	..()
+	if(!src.has_power || !decompiler)
+		return
+
+	//The decompiler replenishes drone stores from hoovered-up junk each tick.
+	for(var/type in decompiler.stored_comms)
+		if(decompiler.stored_comms[type] > 0)
+			var/obj/item/stack/sheet/stack
+			switch(type)
+				if("metal")
+					if(!stack_metal)
+						stack_metal = new(src.module)
+						stack_metal.amount = 1
+					stack = stack_metal
+				if("glass")
+					if(!stack_glass)
+						stack_glass = new(src.module)
+						stack_glass.amount = 1
+					stack = stack_glass
+				if("wood")
+					if(!stack_wood)
+						stack_wood = new(src.module)
+						stack_wood.amount = 1
+					stack = stack_wood
+				if("plastic")
+					if(!stack_plastic)
+						stack_plastic = new(src.module)
+						stack_plastic.amount = 1
+					stack = stack_plastic
+
+			stack.amount++
+			decompiler.stored_comms[type]--;

code/modules/mob/living/silicon/robot/robot_modules.dm
@@ -249,3 +249,72 @@
 		src.emag = new /obj/item/weapon/reagent_containers/spray/alien/acid(src)
 		src.emag.reagents.add_reagent("pacid", 125)
 		src.emag.reagents.add_reagent("sacid", 125)
+
+/obj/item/weapon/robot_module/drone
+	name = "drone module"
+
+
+	New()
+		//TODO: Replace with shittier flashlight and work out why we can't remove the flash. ~Z
+		..()
+		src.modules += new /obj/item/weapon/weldingtool(src)
+		src.modules += new /obj/item/weapon/screwdriver(src)
+		src.modules += new /obj/item/weapon/wrench(src)
+		src.modules += new /obj/item/weapon/crowbar(src)
+		src.modules += new /obj/item/weapon/wirecutters(src)
+		src.modules += new /obj/item/device/multitool(src)
+		src.modules += new /obj/item/device/lightreplacer(src)
+		src.modules += new /obj/item/weapon/reagent_containers/spray/cleaner(src)
+		src.modules += new /obj/item/weapon/gripper(src)
+		src.modules += new /obj/item/weapon/matter_decompiler(src)
+
+		src.emag = new /obj/item/weapon/card/emag(src)
+		src.emag.name = "Cryptographic Sequencer"
+
+
+		var/list/stacktypes = list(
+			/obj/item/stack/rods = 10,
+			/obj/item/stack/tile/plasteel = 10,
+			/obj/item/stack/sheet/metal/cyborg = 10,
+			/obj/item/stack/sheet/wood/cyborg = 1,
+			/obj/item/stack/cable_coil = 30,
+			/obj/item/stack/sheet/glass/cyborg = 10,
+			/obj/item/stack/sheet/mineral/plastic/cyborg = 1
+			)
+
+		for(var/T in stacktypes)
+			var/obj/item/stack/sheet/W = new T(src)
+			W.amount = stacktypes[T]
+			src.modules += W
+
+		return
+
+/obj/item/weapon/robot_module/drone/respawn_consumable(var/mob/living/silicon/robot/R)
+	var/obj/item/weapon/reagent_containers/spray/cleaner/C = locate() in src.modules
+	C.reagents.add_reagent("cleaner", 10)
+
+	var/list/stacks = list (
+		/obj/item/stack/sheet/metal,
+		/obj/item/stack/cable_coil,
+		/obj/item/stack/sheet/glass/cyborg,
+		/obj/item/stack/rods,
+		/obj/item/stack/tile/plasteel
+	)
+
+	for(var/T in stacks)
+		var/O = locate(T) in src.modules
+		var/obj/item/stack/sheet/S = O
+
+		if(!S)
+			src.modules -= null
+			S = new T(src)
+			src.modules += S
+			S.amount = 0
+
+		if(istype(S) && S.amount < 15)
+			S.amount++
+
+	var/obj/item/device/lightreplacer/LR = locate() in src.modules
+	LR.Charge(R)
+
+	return