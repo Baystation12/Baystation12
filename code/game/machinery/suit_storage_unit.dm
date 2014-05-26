TrongleCo/sealab13

Lots of work on hardsuits, commented out some broken WIP code, did some renaming, deleted now-redundant species-specific hardsuits.
Former-commit-id: 4538fbabca45d1176c16b8f7390b3ce00d2debb8

code/game/machinery/suit_storage_unit.dm
@@ -1057,152 +1057,64 @@
 			if(helmet) helmet.species_restricted = list("Tajaran")
 			if(suit) suit.species_restricted = list("Tajaran")
 
-	if(target_department == "Engineering")
-
-		if(helmet) helmet.name = "engineering hardsuit helmet"
-		if(suit) suit.name = "engineering hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-engineering"
-					helmet.item_state = "eng_helm"
-					helmet.item_color = "engineering"
-				if(suit)
-					suit.icon_state = "rig-engineering"
-					suit.item_state = "eng_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				if(helmet)
-					helmet.icon_state = "rig0-taj-helmet"
-					helmet.item_state = "rig0-taj-helmet"
-					helmet.item_color = "taj-helmet"
-				if(suit)
-					suit.icon_state = "rig-taj"
-					suit.item_state = "rig-taj"
-
-	else if(target_department == "Mining")
-
-		if(helmet) helmet.name = "mining hardsuit helmet"
-		if(suit) suit.name = "mining hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-mining"
-					helmet.item_state = "mining_helm"
-					helmet.item_color = "mining"
-				if(suit)
-					suit.icon_state = "rig-mining"
-					suit.item_state = "mining_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "^%###^%$")
-
-		if(helmet) helmet.name = "blood-red hardsuit helmet"
-		if(suit) suit.name = "blood-red hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-human"
-					helmet.item_state = "syndie_helm"
-					helmet.item_color = "syndie-human"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-human"
-			if("Skrell")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-skrell"
-					helmet.item_state = "syndie_helm"
-					helmet.item_color = "syndie-skrell"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-skrell"
-			if("Unathi")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-unathi"
-					helmet.item_state = "syndie_helm"
-					helmet.item_color = "syndie-unathi"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-unathi"
-			if("Tajaran")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-taj"
-					helmet.item_state = "syndie_helm"
-					helmet.item_color = "syndie-taj"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-taj"
-
-	else if(target_department == "Medical")
-
-		if(helmet) helmet.name = "medical hardsuit helmet"
-		if(suit) suit.name = "medical hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-medical"
-					helmet.item_state = "medical_helm"
-					helmet.item_color = "medical"
-				if(suit)
-					suit.icon_state = "rig-medical"
-					suit.item_state = "medical_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "Security")
-
-		if(helmet) helmet.name = "security hardsuit helmet"
-		if(suit) suit.name = "security hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-sec"
-					helmet.item_state = "sec_helm"
-					helmet.item_color = "sec"
-				if(suit)
-					suit.icon_state = "rig-sec"
-					suit.item_state = "sec_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "Atmos")
-
-		if(helmet) helmet.name = "atmospherics hardsuit helmet"
-		if(suit) suit.name = "atmospherics hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-atmos"
-					helmet.item_state = "atmos_helm"
-					helmet.item_color = "atmos"
-				if(suit)
-					suit.icon_state = "rig-atmos"
-					suit.item_state = "atmos_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
+	switch(target_department)
+		if("Engineering")
+			if(helmet)
+				helmet.name = "engineering hardsuit helmet"
+				helmet.icon_state = "rig0-engineering"
+				helmet.item_state = "eng_helm"
+				helmet.item_color = "engineering"
+			if(suit)
+				suit.name = "engineering hardsuit"
+				suit.icon_state = "rig-engineering"
+				suit.item_state = "eng_hardsuit"
+		if("Mining")
+			if(helmet)
+				helmet.name = "mining hardsuit helmet"
+				helmet.icon_state = "rig0-mining"
+				helmet.item_state = "mining_helm"
+				helmet.item_color = "mining"
+			if(suit)
+				suit.name = "mining hardsuit"
+				suit.icon_state = "rig-mining"
+				suit.item_state = "mining_hardsuit"
+		if("Medical")
+			if(helmet)
+				helmet.name = "medical hardsuit helmet"
+				helmet.icon_state = "rig0-medical"
+				helmet.item_state = "medical_helm"
+				helmet.item_color = "medical"
+			if(suit)
+				suit.name = "medical hardsuit"
+				suit.icon_state = "rig-medical"
+				suit.item_state = "medical_hardsuit"
+		if("Security")
+			if(helmet)
+				helmet.name = "security hardsuit helmet"
+				helmet.icon_state = "rig0-sec"
+				helmet.item_state = "sec_helm"
+				helmet.item_color = "sec"
+			if(suit)
+				suit.name = "security hardsuit"
+				suit.icon_state = "rig-sec"
+				suit.item_state = "sec_hardsuit"
+		if("Atmos")
+			if(helmet)
+				helmet.name = "atmospherics hardsuit helmet"
+				helmet.icon_state = "rig0-atmos"
+				helmet.item_state = "atmos_helm"
+				helmet.item_color = "atmos"
+			if(suit)
+				suit.name = "atmospherics hardsuit"
+				suit.icon_state = "rig-atmos"
+				suit.item_state = "atmos_hardsuit"
+		if("^%###^%$")
+			if(helmet)
+				helmet.name = "blood-red hardsuit helmet"
+				helmet.icon_state = "rig0-syndie"
+				helmet.item_state = "syndie_helm"
+				helmet.item_color = "syndie"
+			if(suit)
+				suit.name = "blood-red hardsuit"
+				suit.item_state = "syndie_hardsuit"
+				suit.icon_state = "rig-syndie"

code/modules/clothing/spacesuits/alien.dm
@@ -1,20 +1,4 @@
-// Tajaran rigs.
-/obj/item/clothing/head/helmet/space/rig/tajara
-	desc = "A special helmet designed for work in a hazardous, low-pressure environment. Has radiation shielding. This one doesn't look like it was made for humans."
-	icon_state = "rig0-taj-helmet"
-	item_state = "rig0-taj-helmet"
-	item_color = "taj-helmet"
-	species_restricted = list("Tajaran")
-
-/obj/item/clothing/suit/space/rig/tajara
-	desc = "A special suit that protects against hazardous, low pressure environments. Has radiation shielding. This one doesn't look like it was made for humans."
-	icon_state = "rig-taj"
-	item_state = "rig-taj"
-	item_color = "rig-taj"
-	species_restricted = list("Tajaran")
-
 //Skrell space gear. Sleek like a wetsuit.
-
 /obj/item/clothing/head/helmet/space/skrell
 	name = "Skrellian helmet"
 	desc = "Smoothly contoured and polished to a shine. Still looks like a fishbowl."
@@ -52,7 +36,6 @@
 	item_color = "skrell_suit_black"
 
 //Unathi space gear. Huge and restrictive.
-
 /obj/item/clothing/head/helmet/space/unathi
 	armor = list(melee = 40, bullet = 30, laser = 30,energy = 15, bomb = 35, bio = 100, rad = 50)
 	heat_protection = HEAD
@@ -98,7 +81,6 @@
 
 // Vox space gear (vaccuum suit, low pressure armour)
 // Can't be equipped by any other species due to bone structure and vox cybernetics.
-
 /obj/item/clothing/suit/space/vox
 	w_class = 3
 	allowed = list(/obj/item/weapon/gun,/obj/item/ammo_magazine,/obj/item/ammo_casing,/obj/item/weapon/melee/baton,/obj/item/weapon/melee/energy/sword,/obj/item/weapon/handcuffs,/obj/item/weapon/tank)
@@ -106,12 +88,20 @@
 	armor = list(melee = 60, bullet = 50, laser = 30,energy = 15, bomb = 30, bio = 30, rad = 30)
 	heat_protection = UPPER_TORSO|LOWER_TORSO|LEGS|FEET|ARMS|HANDS
 	max_heat_protection_temperature = SPACE_SUIT_MAX_HEAT_PROTECTION_TEMPERATURE
-	species_restricted = list("Vox")
+	species_restricted = list("Vox", "Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/suit.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/suit.dmi',
+		)
 
 /obj/item/clothing/head/helmet/space/vox
 	armor = list(melee = 60, bullet = 50, laser = 30, energy = 15, bomb = 30, bio = 30, rad = 30)
 	flags = HEADCOVERSEYES|STOPSPRESSUREDMAGE
-	species_restricted = list("Vox")
+	species_restricted = list("Vox","Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/head.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/head.dmi',
+		)
 
 /obj/item/clothing/head/helmet/space/vox/pressure
 	name = "alien helmet"
@@ -130,14 +120,12 @@
 	icon_state = "vox-carapace"
 	item_state = "vox-carapace"
 	desc = "A glowing visor, perhaps stolen from a depressed Cylon."
-	species_restricted = list("Vox","Vox Armalis")
 
 /obj/item/clothing/suit/space/vox/carapace
 	name = "alien carapace armour"
 	icon_state = "vox-carapace"
 	item_state = "vox-carapace"
 	desc = "An armoured, segmented carapace with glowing purple lights. It looks pretty run-down."
-	species_restricted = list("Vox","Vox Armalis")
 
 /obj/item/clothing/head/helmet/space/vox/stealth
 	name = "alien stealth helmet"
@@ -188,16 +176,25 @@
 	item_state = "gloves-vox"
 	siemens_coefficient = 0
 	permeability_coefficient = 0.05
-	item_color="gloves-vox"
+	item_color = "gloves-vox"
 	species_restricted = list("Vox","Vox Armalis")
-
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/gloves.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/gloves.dmi',
+		)
 /obj/item/clothing/shoes/magboots/vox
 
 	desc = "A pair of heavy, jagged armoured foot pieces, seemingly suitable for a velociraptor."
 	name = "vox magclaws"
 	item_state = "boots-vox"
 	icon_state = "boots-vox"
+
 	species_restricted = list("Vox","Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/feet.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/feet.dmi',
+		)
+
 	action_button_name = "Toggle the magclaws"
 
 /obj/item/clothing/shoes/magboots/vox/attack_self(mob/user)
@@ -236,49 +233,3 @@
 	..()
 	if (magpulse)
 		usr << "It would be hard to take these off without relaxing your grip first." //theoretically this message should only be seen by the wearer when the claws are equipped.
-
-//Species-specific Syndicate rigs.
-
-/obj/item/clothing/head/helmet/space/rig/syndi/tajara
-	icon_state = "rig0-syndie-taj"
-	item_state = "syndie_helm"
-	item_color = "syndie-taj"
-	species_restricted = list("Tajaran")
-
-/obj/item/clothing/suit/space/rig/syndi/tajara
-	item_state = "syndie_hardsuit"
-	icon_state = "rig-syndie-taj"
-	species_restricted = list("Tajaran")
-
-/obj/item/clothing/head/helmet/space/rig/syndi/unathi
-	icon_state = "rig0-syndie-unathi"
-	item_state = "syndie_helm"
-	item_color = "syndie-unathi"
-	species_restricted = list("Unathi")
-
-/obj/item/clothing/suit/space/rig/syndi/unathi
-	item_state = "syndie_hardsuit"
-	icon_state = "rig-syndie-unathi"
-	species_restricted = list("Unathi")
-
-/obj/item/clothing/head/helmet/space/rig/syndi/skrell
-	icon_state = "rig0-syndie-skrell"
-	item_state = "syndie_helm"
-	item_color = "syndie-skrell"
-	species_restricted = list("Skrell")
-
-/obj/item/clothing/suit/space/rig/syndi/skrell
-	item_state = "syndie_hardsuit"
-	icon_state = "rig-syndie-skrell"
-	species_restricted = list("Skrell")
-
-/obj/item/clothing/head/helmet/space/rig/syndi/human
-	icon_state = "rig0-syndie-human"
-	item_state = "syndie_helm"
-	item_color = "syndie-human"
-	species_restricted = list("Human")
-
-/obj/item/clothing/suit/space/rig/syndi/human
-	item_state = "syndie_hardsuit"
-	icon_state = "rig-syndie-human"
-	species_restricted = list("Human")

Baystation12/Baystation12

Conflicts:
	code/game/machinery/suit_storage_unit.dm
	code/game/objects/items.dm
	code/game/objects/items/devices/modkit.dm
	code/modules/clothing/masks/breath.dm
	code/modules/clothing/spacesuits/alien.dm
	code/modules/clothing/spacesuits/rig.dm

code/game/machinery/suit_storage_unit.dm
@@ -1056,152 +1056,64 @@
 			if(helmet) helmet.species_restricted = list("Tajaran")
 			if(suit) suit.species_restricted = list("Tajaran")
 
-	if(target_department == "Engineering")
-
-		if(helmet) helmet.name = "engineering hardsuit helmet"
-		if(suit) suit.name = "engineering hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-engineering"
-					helmet.item_state = "eng_helm"
-					helmet._color = "engineering"
-				if(suit)
-					suit.icon_state = "rig-engineering"
-					suit.item_state = "eng_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				if(helmet)
-					helmet.icon_state = "rig0-taj-helmet"
-					helmet.item_state = "rig0-taj-helmet"
-					helmet._color = "taj-helmet"
-				if(suit)
-					suit.icon_state = "rig-taj"
-					suit.item_state = "rig-taj"
-
-	else if(target_department == "Mining")
-
-		if(helmet) helmet.name = "mining hardsuit helmet"
-		if(suit) suit.name = "mining hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-mining"
-					helmet.item_state = "mining_helm"
-					helmet._color = "mining"
-				if(suit)
-					suit.icon_state = "rig-mining"
-					suit.item_state = "mining_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "^%###^%$")
-
-		if(helmet) helmet.name = "blood-red hardsuit helmet"
-		if(suit) suit.name = "blood-red hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-human"
-					helmet.item_state = "syndie_helm"
-					helmet._color = "syndie-human"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-human"
-			if("Skrell")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-skrell"
-					helmet.item_state = "syndie_helm"
-					helmet._color = "syndie-skrell"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-skrell"
-			if("Unathi")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-unathi"
-					helmet.item_state = "syndie_helm"
-					helmet._color = "syndie-unathi"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-unathi"
-			if("Tajaran")
-				if(helmet)
-					helmet.icon_state = "rig0-syndie-taj"
-					helmet.item_state = "syndie_helm"
-					helmet._color = "syndie-taj"
-				if(suit)
-					suit.item_state = "syndie_hardsuit"
-					suit.icon_state = "rig-syndie-taj"
-
-	else if(target_department == "Medical")
-
-		if(helmet) helmet.name = "medical hardsuit helmet"
-		if(suit) suit.name = "medical hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-medical"
-					helmet.item_state = "medical_helm"
-					helmet._color = "medical"
-				if(suit)
-					suit.icon_state = "rig-medical"
-					suit.item_state = "medical_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "Security")
-
-		if(helmet) helmet.name = "security hardsuit helmet"
-		if(suit) suit.name = "security hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-sec"
-					helmet.item_state = "sec_helm"
-					helmet._color = "sec"
-				if(suit)
-					suit.icon_state = "rig-sec"
-					suit.item_state = "sec_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
-
-	else if(target_department == "Atmos")
-
-		if(helmet) helmet.name = "atmospherics hardsuit helmet"
-		if(suit) suit.name = "atmospherics hardsuit"
-
-		switch(target_species)
-			if("Human")
-				if(helmet)
-					helmet.icon_state = "rig0-atmos"
-					helmet.item_state = "atmos_helm"
-					helmet._color = "atmos"
-				if(suit)
-					suit.icon_state = "rig-atmos"
-					suit.item_state = "atmos_hardsuit"
-			if("Skrell")
-				return //TODO
-			if("Unathi")
-				return //TODO
-			if("Tajaran")
-				return //TODO
+	switch(target_department)
+		if("Engineering")
+			if(helmet)
+				helmet.name = "engineering hardsuit helmet"
+				helmet.icon_state = "rig0-engineering"
+				helmet.item_state = "eng_helm"
+				helmet._color = "engineering"
+			if(suit)
+				suit.name = "engineering hardsuit"
+				suit.icon_state = "rig-engineering"
+				suit.item_state = "eng_hardsuit"
+		if("Mining")
+			if(helmet)
+				helmet.name = "mining hardsuit helmet"
+				helmet.icon_state = "rig0-mining"
+				helmet.item_state = "mining_helm"
+				helmet._color = "mining"
+			if(suit)
+				suit.name = "mining hardsuit"
+				suit.icon_state = "rig-mining"
+				suit.item_state = "mining_hardsuit"
+		if("Medical")
+			if(helmet)
+				helmet.name = "medical hardsuit helmet"
+				helmet.icon_state = "rig0-medical"
+				helmet.item_state = "medical_helm"
+				helmet._color = "medical"
+			if(suit)
+				suit.name = "medical hardsuit"
+				suit.icon_state = "rig-medical"
+				suit.item_state = "medical_hardsuit"
+		if("Security")
+			if(helmet)
+				helmet.name = "security hardsuit helmet"
+				helmet.icon_state = "rig0-sec"
+				helmet.item_state = "sec_helm"
+				helmet._color = "sec"
+			if(suit)
+				suit.name = "security hardsuit"
+				suit.icon_state = "rig-sec"
+				suit.item_state = "sec_hardsuit"
+		if("Atmos")
+			if(helmet)
+				helmet.name = "atmospherics hardsuit helmet"
+				helmet.icon_state = "rig0-atmos"
+				helmet.item_state = "atmos_helm"
+				helmet._color = "atmos"
+			if(suit)
+				suit.name = "atmospherics hardsuit"
+				suit.icon_state = "rig-atmos"
+				suit.item_state = "atmos_hardsuit"
+		if("^%###^%$")
+			if(helmet)
+				helmet.name = "blood-red hardsuit helmet"
+				helmet.icon_state = "rig0-syndie"
+				helmet.item_state = "syndie_helm"
+				helmet._color = "syndie"
+			if(suit)
+				suit.name = "blood-red hardsuit"
+				suit.item_state = "syndie_hardsuit"
+				suit.icon_state = "rig-syndie"

code/modules/clothing/spacesuits/alien.dm
@@ -14,7 +14,6 @@
 	species_restricted = list("Tajaran")
 
 //Skrell space gear. Sleek like a wetsuit.
-
 /obj/item/clothing/head/helmet/space/skrell
 	name = "Skrellian helmet"
 	desc = "Smoothly contoured and polished to a shine. Still looks like a fishbowl."
@@ -52,10 +51,8 @@
 	_color = "skrell_suit_black"
 
 
-
 // Vox space gear (vaccuum suit, low pressure armour)
 // Can't be equipped by any other species due to bone structure and vox cybernetics.
-
 /obj/item/clothing/suit/space/vox
 	w_class = 3
 	allowed = list(/obj/item/weapon/gun,/obj/item/ammo_box,/obj/item/ammo_casing,/obj/item/weapon/melee/baton,/obj/item/weapon/melee/energy/sword,/obj/item/weapon/handcuffs,/obj/item/weapon/tank)
@@ -63,12 +60,20 @@
 	armor = list(melee = 60, bullet = 50, laser = 30,energy = 15, bomb = 30, bio = 30, rad = 30)
 	heat_protection = UPPER_TORSO|LOWER_TORSO|LEGS|FEET|ARMS|HANDS
 	max_heat_protection_temperature = SPACE_SUIT_MAX_HEAT_PROTECITON_TEMPERATURE
-	species_restricted = list("Vox")
+	species_restricted = list("Vox", "Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/suit.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/suit.dmi',
+		)
 
 /obj/item/clothing/head/helmet/space/vox
 	armor = list(melee = 60, bullet = 50, laser = 30, energy = 15, bomb = 30, bio = 30, rad = 30)
 	flags = HEADCOVERSEYES|STOPSPRESSUREDMAGE
-	species_restricted = list("Vox")
+	species_restricted = list("Vox","Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/head.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/head.dmi',
+		)
 
 /obj/item/clothing/head/helmet/space/vox/pressure
 	name = "alien helmet"
@@ -143,19 +148,25 @@
 	item_state = "gloves-vox"
 	siemens_coefficient = 0
 	permeability_coefficient = 0.05
-	_color="gloves-vox"
+	_color = "gloves-vox"
 	species_restricted = list("Vox","Vox Armalis")
-	species_fit = list("Vox Armalis")
-
-
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/gloves.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/gloves.dmi',
+		)
 /obj/item/clothing/shoes/magboots/vox
 
 	desc = "A pair of heavy, jagged armoured foot pieces, seemingly suitable for a velociraptor."
 	name = "vox magclaws"
 	item_state = "boots-vox"
 	icon_state = "boots-vox"
+
 	species_restricted = list("Vox","Vox Armalis")
-	species_fit = list("Vox Armalis")
+	sprite_sheets = list(
+		"Vox" = 'icons/mob/species/vox/feet.dmi',
+		"Vox Armalis" = 'icons/mob/species/armalis/feet.dmi',
+		)
+
 	action_button_name = "Toggle the magclaws"
 
 /obj/item/clothing/shoes/magboots/vox/attack_self(mob/user)
@@ -193,42 +204,6 @@
 		usr << "It would be hard to take these off without relaxing your grip first." //theoretically this message should only be seen by the wearer when the claws are equipped.
 
 
-//Vox Armalis gear.
-//Vox Armalis gear.
-/obj/item/clothing/shoes/magboots/vox/armalis
- 	name = "large vox magclaws"
- 	item_state = "boots-armalis"
- 	icon_state = "boots-armalis"
- 	icon_override = 'icons/mob/species/vox/armalis/shoes.dmi'
- 	species_restricted = list("Vox Armalis")
-
-/obj/item/clothing/gloves/yellow/vox/armalis
- 	name = "large insulated gauntlets"
- 	item_state = "gloves-armalis"
- 	icon_state = "gloves-armalis"
- 	icon_override = 'icons/mob/species/vox/armalis/hands.dmi'
- 	species_restricted = list("Vox Armalis")
-
-/obj/item/clothing/mask/breath/vox/armalis
- 	name = "large vox mask"
- 	item_state = "mask-armalis"
- 	icon_state = "mask-armalis"
- 	icon_override = 'icons/mob/species/vox/armalis/mask.dmi'
- 	species_restricted = list("Vox Armalis")
-
-/obj/item/clothing/suit/space/vox/carapace/armalis
-	name = "large alien carapace armour"
-	item_state = "armour-armalis"
-	icon_state = "armour-armalis"
-	icon_override = 'icons/mob/vox.dmi'
-	species_restricted = list("Vox Armalis")
-
-/obj/item/clothing/suit/space/vox/carapace/armalis
-	name = "large alien carapace armour"
-	item_state = "armour-armalis"
-	icon_state = "armour-armalis"
-	icon_override = 'icons/mob/vox.dmi'
-	species_restricted = list("Vox Armalis")
 
 //Species-specific Syndicate rigs.
 /obj/item/clothing/head/helmet/space/rig/syndi/tajara
@@ -298,6 +273,7 @@
 	icon_state = "rig-syndie-unathi"
 	species_restricted = list("Unathi")
 
+//Unathi space gear. Huge and restrictive.
 /obj/item/clothing/head/helmet/space/unathi
 	armor = list(melee = 40, bullet = 30, laser = 30,energy = 15, bomb = 35, bio = 100, rad = 50)
 	heat_protection = HEAD