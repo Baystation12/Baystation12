057ed2017307a1b9895d75739554ecf43f0f3f2e
ZomgPonies <lorden@zomgponies.com>
2014-06-21 05:33:32 -0400

    Yeah fuck this tools folder, installed BYONDtools directly into python instead.

--- tools/BYONDTools/doc/build_html_docs.bat
+++
@@ -1,3 +0,0 @@
-rm -rf _build
-call make html
-pause
--- tools/BYONDTools/tests/runtests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from Atom import *
-from MapParser import *
-from MapRendering import *
-from ObjectTree import *
-
-if __name__ == "__main__":
-    unittest.main()
