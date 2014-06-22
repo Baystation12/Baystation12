TrongleCo/sealab13

Fixes FEA temperature_expose() being used...
...instead of ZAS fire_act().


Former-commit-id: 37e63dfcb08e9bf2473f1d78e03deab3e5b98ca1

code/game/objects/structures/window.dm
@@ -361,7 +361,7 @@
 
 		return
 
-/obj/structure/window/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+/obj/structure/window/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > T0C + 800)
 		hit(round(exposed_volume / 100), 0)
 	..()
@@ -381,7 +381,7 @@
 	shardtype = /obj/item/weapon/shard/phoron
 	health = 120
 
-/obj/structure/window/phoronbasic/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+/obj/structure/window/phoronbasic/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > T0C + 32000)
 		hit(round(exposed_volume / 1000), 0)
 	..()
@@ -395,7 +395,7 @@
 	reinf = 1
 	health = 160
 
-/obj/structure/window/phoronreinforced/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+/obj/structure/window/phoronreinforced/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	return
 
 /obj/structure/window/reinforced

code/game/turfs/simulated/walls_mineral.dm
@@ -111,7 +111,7 @@
 	for(var/obj/machinery/door/airlock/phoron/D in range(3,src))
 		D.ignite(temperature/4)
 
-/turf/simulated/wall/mineral/phoron/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)//Doesn't fucking work because walls don't interact with air :(
+/turf/simulated/wall/mineral/phoron/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)//Doesn't fucking work because walls don't interact with air :(
 	if(exposed_temperature > 300)
 		PhoronBurn(exposed_temperature)
 

Baystation12/Baystation12

Conflicts:
	code/game/gamemodes/blob/theblob.dm
	code/game/machinery/doors/airlock.dm
	code/game/objects/structures/window.dm
	code/game/turfs/simulated/walls_mineral.dm
	code/modules/mob/living/carbon/alien/special/facehugger.dm
	code/modules/reagents/reagent_dispenser.dm

code/game/objects/structures/window.dm
@@ -365,7 +365,7 @@
 /obj/structure/window/update_icon()
 	return
 
-/obj/structure/window/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+/obj/structure/window/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > T0C + 800)
 		hit(round(exposed_volume / 100), 0)
 	..()
@@ -391,7 +391,7 @@
 	update_nearby_icons()
 	return
 
-/obj/structure/window/plasmabasic/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+/obj/structure/window/plasmabasic/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > T0C + 32000)
 		hit(round(exposed_volume / 1000), 0)
 	..()
@@ -406,8 +406,6 @@
 	health = 160
 	explosion_resistance = 4
 
-/obj/structure/window/plasmareinforced/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
-	return
 
 /obj/structure/window/plasmareinforced/New(Loc,re=0)
 	..()
@@ -415,6 +413,9 @@
 	color = null
 	update_nearby_tiles(need_rebuild=1)
 	update_nearby_icons()
+	return
+
+/obj/structure/window/plasmareinforced/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	return
 
 /obj/structure/window/reinforced

code/game/turfs/simulated/walls_mineral.dm
@@ -114,7 +114,7 @@
 	for(var/obj/machinery/door/airlock/plasma/D in range(3,src))
 		D.ignite(temperature/4)
 
-/turf/simulated/wall/mineral/plasma/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)//Doesn't fucking work because walls don't interact with air :(
+/turf/simulated/wall/mineral/plasma/fire_act(datum/gas_mixture/air, exposed_temperature, exposed_volume)//Doesn't fucking work because walls don't interact with air :(
 	if(exposed_temperature > 300)
 		PlasmaBurn(exposed_temperature)
 