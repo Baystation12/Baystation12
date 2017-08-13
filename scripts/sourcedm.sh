112bbbfd3cb18df305e2566eb117e9cf6b13f5b3
Colm Ó hIcí <colmohici@gmail.com>
2017-08-13 14:13:28 +0100

    add support for WSL to sourcedm

--- scripts/sourcedm.sh
+++ scripts/sourcedm.sh
@@ -15,6 +15,21 @@ then
         export DM='/c/Program Files/BYOND/bin/dm.exe'
         return 0
     fi
+elif grep -Fq "Microsoft" /proc/sys/kernel/osrelease # detect WSL
+then
+    if hash dm.exe 2>/dev/null
+    then
+        export DM='dm.exe'
+        return 0
+    elif [[ -a '/mnt/c/Program Files (x86)/BYOND/bin/dm.exe' ]]
+    then
+        export DM='/mnt/c/Program Files (x86)/BYOND/bin/dm.exe'
+        return 0
+    elif [[ -a '/mnt/c/Program Files/BYOND/bin/dm.exe' ]]
+    then
+        export DM='/mnt/c/Program Files/BYOND/bin/dm.exe'
+        return 0
+    fi
 else
     if hash DreamMaker 2>/dev/null
     then
