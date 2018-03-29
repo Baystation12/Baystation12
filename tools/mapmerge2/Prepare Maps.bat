28618ac3aca57806a93ca63dca2217e74b32d175
FrozenGuy5 <spriteespritee@gmail.com>
2018-03-29 15:30:06 +0100

    Mapmerge2 & mapmerges most of every map
    The only maps that haven't been mapmerged are magshield & marooned because of some error.

--- tools/mapmerge2/Prepare Maps.bat
+++ tools/mapmerge2/Prepare Maps.bat
@@ -1,5 +1,5 @@
 @echo off
-cd ../../../maps/
+cd ../../maps/
 
 for /R %%f in (*.dmm) do copy "%%f" "%%f.backup"
 
@@ -7,6 +7,6 @@ cls
 echo All dmm files in maps directories have been backed up
 echo Now you can make your changes...
 echo ---
-echo Remember to run Run_Map_Merge.bat just before you commit your changes!
+echo Remember to run mapmerge.bat just before you commit your changes!
 echo ---
 pause
