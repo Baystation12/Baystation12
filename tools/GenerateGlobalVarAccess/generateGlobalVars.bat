33450890e029ec1b4d8864e1bd7cd821bd2fac31
Ingredients <wesmdow@gmail.com>
2018-06-21 18:35:31 -0500

    Fixing Path

--- tools/GenerateGlobalVarAccess/generateGlobalVars.bat
+++ tools/GenerateGlobalVarAccess/generateGlobalVars.bat
@@ -1,3 +1,3 @@
 @echo off
-call C:\Python27\python.exe gen_globals.py ../../baystation12.dme ../../code/_helpers/global_access.dm
+call "C:\Users\Harishini's PC\AppData\Local\Programs\Python\Python36-32\python.exe" gen_globals.py ../../baystation12.dme ../../code/_helpers/global_access.dm
 pause
