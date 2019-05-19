c9eb06cecbefdc22fc03e90689d4623152257056
WhityTaty <29167103+WhityTaty@users.noreply.github.com>
2019-05-19 05:37:36 +0300

    321123

--- nano/To BYOND Cache.bat
+++
@@ -1,4 +0,0 @@
-copy css\* "%USERPROFILE%\Documents\BYOND\cache" /y
-copy images\* "%USERPROFILE%\Documents\BYOND\cache" /y
-copy js\* "%USERPROFILE%\Documents\BYOND\cache" /y
-copy templates\* "%USERPROFILE%\Documents\BYOND\cache" /y
