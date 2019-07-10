e7d5a25bf623e07a58b7ae554f112b1334fd7b6b
Jacob Greenfield <xales@naveria.com>
2019-07-10 00:38:16 -0400

    Update globals checksum

--- test/run-test.sh
+++ test/run-test.sh
@@ -228,7 +228,7 @@ function run_byond_tests {
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
     run_test_ci "check globals build" "python3 tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
-    run_test "check globals unchanged" "md5sum -c - <<< '466824444e466f3ca1eeb1e24d331dd3 *code/_helpers/global_access.dm'"
+    run_test "check globals unchanged" "md5sum -c - <<< '3a0de8c84b0e35ba37ae4701c68bdcf7 *code/_helpers/global_access.dm'"
     run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon baystation12.dmb -invisible -trusted -core 2>&1 | tee log.txt"
