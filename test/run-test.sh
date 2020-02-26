4426c7babf54054090f78b2af5e1c03b289a0f84
Eluxor <38334353+Eluxor@users.noreply.github.com>
2020-02-26 17:56:55 -0300

    Runtest updoot and Slayer

--- test/run-test.sh
+++ test/run-test.sh
@@ -240,7 +240,7 @@ function run_byond_tests {
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
     run_test_ci "check globals build" "python tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
-    run_test "check globals unchanged" "md5sum -c - <<< '8180559decc147a0ad40e65b936bed45 *code/_helpers/global_access.dm'"
+    run_test "check globals unchanged" "md5sum -c - <<< '7460e35a86d43d25e42ce2afe7a51ab4 *code/_helpers/global_access.dm'"
     run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon baystation12.dmb -invisible -trusted -core 2>&1 | tee log.txt"
