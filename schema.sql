61ece4e4d08fd81ed75d091dd3512d2a833deb87
bitMuse <ddffdsa@afdasda.com>
2020-03-22 13:52:58 -0700

    Updated schema file for persistence db.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.3.9-MariaDB - mariadb.org binary distribution
+-- Server version:               8.0.16 - MySQL Community Server - GPL
 -- Server OS:                    Win64
--- HeidiSQL Version:             9.4.0.5125
+-- HeidiSQL Version:             9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,38 +11,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
--- Dumping database structure for persistent
-CREATE DATABASE IF NOT EXISTS `persistent` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `persistent`;
-
 -- Dumping structure for table persistent.list
 CREATE TABLE IF NOT EXISTS `list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `length` int(11) NOT NULL DEFAULT 0,
-  `version` int(11) NOT NULL DEFAULT 1,
+  `id` int(11) NOT NULL,
+  `length` int(11) NOT NULL DEFAULT '0',
+  `version` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 -- Dumping structure for table persistent.list_element
 CREATE TABLE IF NOT EXISTS `list_element` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `list_id` int(11) NOT NULL,
   `index` int(11) NOT NULL,
-  `key` varchar(64) NOT NULL,
-  `key_type` varchar(64) NOT NULL,
-  `value` varchar(2048) NOT NULL,
-  `value_type` varchar(64) NOT NULL,
-  `version` int(11) NOT NULL DEFAULT 1,
+  `key` longtext NOT NULL,
+  `key_type` varchar(256) NOT NULL,
+  `value` longtext NOT NULL,
+  `value_type` varchar(256) NOT NULL,
+  `version` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 -- Dumping structure for table persistent.thing
 CREATE TABLE IF NOT EXISTS `thing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(64) NOT NULL,
+  `id` int(11) NOT NULL,
+  `type` varchar(256) NOT NULL,
   `x` int(11) DEFAULT NULL,
   `y` int(11) DEFAULT NULL,
   `z` int(11) DEFAULT NULL,
@@ -53,11 +48,11 @@ CREATE TABLE IF NOT EXISTS `thing` (
 -- Data exporting was unselected.
 -- Dumping structure for table persistent.thing_var
 CREATE TABLE IF NOT EXISTS `thing_var` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `thing_id` int(11) NOT NULL,
-  `key` varchar(64) NOT NULL,
-  `type` varchar(64) NOT NULL,
-  `value` varchar(2048) NOT NULL,
+  `key` varchar(256) NOT NULL,
+  `type` varchar(256) NOT NULL,
+  `value` longtext NOT NULL,
   `version` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
