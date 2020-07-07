ceaf7f8dbc6ccc15051e34e71436308161cf08c8
spookerton <spkrtn@pm.me>
2020-07-07 19:52:14 +0100

    update checks

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -30,11 +30,11 @@ exactly 2 "/datum text paths" '"/datum'
 exactly 2 "/mob text paths" '"/mob'
 exactly 10 "/obj text paths" '"/obj'
 exactly 8 "/turf text paths" '"/turf'
-exactly 131 "to_world uses" '\sto_world\('
+exactly 129 "to_world uses" '\sto_world\('
 exactly 65 "to_world_log uses" '\sto_world_log\('
-exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
-exactly 2 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
-exactly 209 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 0 "world<< uses" 'world<<|world[[:space:]]<<'
+exactly 0 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
+exactly 105 "<< uses" '(?<!<)<<(?!<)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
 exactly 24 "text2path uses" 'text2path'
 exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
