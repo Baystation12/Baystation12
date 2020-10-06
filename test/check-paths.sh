a45db2806204ca3d19fb1e1bef8aa5d59873e636
AlexMorgan3817 <Ivan.Zvereff@gmail.com>
2020-10-06 11:54:32 +0700

    fix Travis

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -41,9 +41,9 @@ exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
 exactly 1 "goto use" 'goto '
 exactly 487 "spawn uses" 'spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
 exactly 0 "tag uses" '\stag = ' -P '**/*.dmm'
-exactly 363 "/const/ vars defined" '/const/'
-exactly 239 "/global/ or /static/ vars defined" '/(global|static)/' -P
-exactly 680 "bad defined global vars" '^(/|())var/' -P
+exactly 365 "/const/ vars defined" '/const/'
+exactly 241 "/global/ or /static/ vars defined" '/(global|static)/' -P
+exactly 684 "bad defined global vars" '^(/|())var/' -P
 # With the potential exception of << if you increase any of these numbers you're probably doing it wrong
 
 broken_files=0
