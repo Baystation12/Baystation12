0e5bf263a806d1e8029ebac2fd9ace240804409a
Gregory Maddra <bloxgate@gmail.com>
2020-11-11 19:06:54 -0600

    Fix map check regex

--- scripts/validateTravisContainsAllMaps.sh
+++ scripts/validateTravisContainsAllMaps.sh
@@ -7,7 +7,7 @@ fail=0
 
 for k in maps/*; do
 	map=${k#maps/}
-	if [[ -e maps/$map/$map.dm ]] && ! grep "\s*MAPPATH: \[[\s\w,]*${map}(?:\]|,[\s\w]*\])" .github/workflows/tests.yml > /dev/null; then
+	if [[ -e maps/$map/$map.dm ]] && ! grep "\s*MAPPATH: \[[\s\w,]*${map}(?:\]|,[\s\w,]*\])" .github/workflows/tests.yml > /dev/null; then
 		# $map is a valid map key, but travis isn't testing it!
 		fail=$((fail + 1))
 		echo "Map key '$map' is present in the repository, but is not listed in .github/workflows/tests.yml!"
