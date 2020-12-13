fc95e704f67ec65bfe1034b763188f8503703bfa
Gregory Maddra <bloxgate@gmail.com>
2020-12-13 14:27:04 -0600

    Make global variable generation robust against line ending differences

--- tools/GenerateGlobalVarAccess/gen_globals.py
+++ tools/GenerateGlobalVarAccess/gen_globals.py
@@ -76,7 +76,7 @@ def main():
 def GenerateMD5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+        for chunk in iter(lambda: f.read(4096).replace(b"\r\n", b"\n"), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
