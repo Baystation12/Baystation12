e4d3c02b24723b4b97087b9a3d2b388be5bf9224
PsiOmegaDelta <git@psiomega.se>
2021-03-20 21:11:38 +0100

    Add support for both cl text and emoji

--- tools/changelog/generate_cl.py
+++ tools/changelog/generate_cl.py
@@ -28,7 +28,7 @@ from pathlib import Path
 from ruamel import yaml
 from github import Github, InputGitAuthor
 
-CL_BODY = re.compile(r":cl:(.+)?\r\n((.|\n|\r)+?)\r\n\/:cl:", re.MULTILINE)
+CL_BODY = re.compile(r"(:cl:|🆑)(.+)?\r\n((.|\n|\r)+?)\r\n\/(:cl:|🆑)", re.MULTILINE)
 CL_SPLIT = re.compile(r"(^\w+):\s+(\w.+)", re.MULTILINE)
 
 git_email = os.getenv("GIT_EMAIL")
@@ -57,14 +57,14 @@ pr_author = pr.user.login
 write_cl = {}
 try:
     cl = CL_BODY.search(pr_body)
-    cl_list = CL_SPLIT.findall(cl.group(2))
+    cl_list = CL_SPLIT.findall(cl.group(3))
 except AttributeError:
     print("No CL found!")
     exit(0) # Change to '0' if you do not want the action to fail when no CL is provided
 
 
-if cl.group(1) is not None:
-    write_cl['author'] = cl.group(1).lstrip()
+if cl.group(2) is not None:
+    write_cl['author'] = cl.group(2).lstrip()
 else:
     write_cl['author'] = pr_author
 
