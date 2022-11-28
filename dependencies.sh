84a9702ec46330f41f8a5252bad959f1be9b42a2
Archemagus <32466328+Archemagus@users.noreply.github.com>
2022-11-29 02:15:59 +0300

    Пытаемся убрать лишние зависимости

--- dependencies.sh
+++ dependencies.sh
@@ -9,20 +9,20 @@ export BYOND_MAJOR=514
 export BYOND_MINOR=1557
 
 #rust_g git tag
-export RUST_G_VERSION=0.5.0
+export RUST_G_VERSION=1.2.0
 
 #bsql git tag (Not sure that needed ~Laxesh)
-export BSQL_VERSION=v1.4.0.0
+#export BSQL_VERSION=v1.4.0.0 Not Used
 
 #node version (Not sure that needed ~Laxesh)
-export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+#export NODE_VERSION=14
+#export NODE_VERSION_PRECISE=14.16.1
 
 # PHP version (Not sure that needed ~Laxesh)
-export PHP_VERSION=5.6
+#export PHP_VERSION=5.6
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.4
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.7.9
+#export PYTHON_VERSION=3.7.9
