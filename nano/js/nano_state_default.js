4b1904559b6022c41c9b07cd27365a63a370cf44
Sierra Helper <125094432+SierraHelper@users.noreply.github.com>
2023-04-30 13:32:13 +0300

    [MIRROR] nano housekeeping 1 (#460)
    Co-authored-by: Spookerton <918997+Spookerton@users.noreply.github.com>

--- nano/js/nano_state_default.js
+++ nano/js/nano_state_default.js
@@ -1,14 +1,8 @@
-
-NanoStateDefaultClass.inheritsFrom(NanoStateClass);
-var NanoStateDefault = new NanoStateDefaultClass();
+NanoStateDefaultClass.inheritsFrom(NanoStateClass)
+var NanoStateDefault = new NanoStateDefaultClass()
 
 function NanoStateDefaultClass() {
-
-    this.key = 'default';
-
-    //this.parent.constructor.call(this);
-
-    this.key = this.key.toLowerCase();
-
-    NanoStateManager.addState(this);
+  this.key = 'default'
+  this.key = this.key.toLowerCase()
+  NanoStateManager.addState(this)
 }
