709961421ea033761656289d16c4cde8ec998ead
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-05-03 01:28:48 +0300

    [MIRROR] Mysql schema dump (#490)
    Co-authored-by: Sierra Komodo Brown <sierrakomodo@hey.com>

--- sql/migrate/V008__Add_Discord_Accounts.sql
+++
@@ -1,6 +0,0 @@
-
---
--- Table structure for table `ckey_whitelist`
---
-ALTER TABLE `erro_player` ADD `discord_id` varchar(32) DEFAULT NULL AFTER `staffwarn`;
-ALTER TABLE `erro_player` ADD `discord_name` varchar(32) DEFAULT NULL AFTER `discord_id`;
