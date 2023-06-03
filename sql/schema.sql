5e34da33424ed8cd4b83e9fb8a975a3cf71b37a3
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-06-03 19:58:23 +0300

    DB: Everything `utf8mb4_unicode_ci` (#624)

--- sql/schema.sql
+++ sql/schema.sql
@@ -88,42 +88,42 @@ CREATE TABLE IF NOT EXISTS `whitelist` (
 */
 CREATE TABLE IF NOT EXISTS `erro_admin_tickets` (
   `id` int(11) AUTO_INCREMENT,
-  `assignee` text DEFAULT NULL,
-  `ckey` varchar(32) NOT NULL,
-  `text` text DEFAULT NULL,
-  `status` enum('OPEN','CLOSED','SOLVED','TIMED_OUT') NOT NULL,
-  `round` varchar(32),
+  `assignee` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('OPEN','CLOSED','SOLVED','TIMED_OUT') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `round` varchar(32) COLLATE utf8mb4_unicode_ci,
   `inround_id` int(11),
   `open_date` date,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS `erro_connection_log`
 (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`datetime` DATETIME DEFAULT now() NOT NULL,
-	`serverip` VARCHAR(32) NOT NULL,
-	`server_port` INT(5) UNSIGNED NOT NULL,
-	`ckey` VARCHAR(32) NOT NULL,
-	`ip` VARCHAR(32) NOT NULL,
-	`computerid` VARCHAR(32) NOT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`datetime` datetime DEFAULT now() NOT NULL,
+	`serverip` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`server_port` int(5) UNSIGNED NOT NULL,
+	`ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`ip` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`computerid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS `ckey_whitelist`
 (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`date` DATETIME DEFAULT now() NOT NULL,
-	`ckey` VARCHAR(32) NOT NULL,
-	`adminwho` VARCHAR(32) NOT NULL,
-	`port` INT(5) UNSIGNED NOT NULL,
-	`date_start` DATETIME DEFAULT now() NOT NULL,
-	`date_end` DATETIME NULL,
-	`is_valid` BOOLEAN DEFAULT true NOT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`date` datetime DEFAULT now() NOT NULL,
+	`ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`adminwho` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`port` int(5) UNSIGNED NOT NULL,
+	`date_start` datetime DEFAULT now() NOT NULL,
+	`date_end` datetime NULL,
+	`is_valid` boolean DEFAULT true NOT NULL,
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `erro_player` ADD `discord_id` varchar(32) DEFAULT NULL AFTER `staffwarn`;
 ALTER TABLE `erro_player` ADD `discord_name` varchar(32) DEFAULT NULL AFTER `discord_id`;
@@ -132,25 +132,25 @@ ALTER TABLE `erro_player` ADD `exp` text DEFAULT NULL AFTER `discord_name`;
 ALTER TABLE `erro_player` ADD `species_exp` text DEFAULT NULL AFTER `exp`;
 
 CREATE TABLE IF NOT EXISTS `erro_playtime_history` (
-  `ckey` VARCHAR(32) NOT NULL,
-  `date` DATE NOT NULL,
-  `time_living` INT(32) NOT NULL DEFAULT '0',
-  `time_ghost` INT(32) NOT NULL DEFAULT '0',
+  `ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time_living` int(32) NOT NULL DEFAULT '0',
+  `time_ghost` int(32) NOT NULL DEFAULT '0',
   CONSTRAINT PRIMARY KEY (ckey, date)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `budget`
 (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`date` datetime DEFAULT now() NOT NULL,
-	`ckey` varchar(32) NOT NULL,
+	`ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
 	`amount` int(10) UNSIGNED NOT NULL,
-	`source` varchar(32) NOT NULL,
+	`source` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
 	`date_start` datetime DEFAULT now() NOT NULL,
 	`date_end` datetime DEFAULT (now() + INTERVAL 1 MONTH),
 	`is_valid` boolean DEFAULT true NOT NULL,
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /* SS220 ban table */
 ALTER TABLE `ban`
@@ -174,3 +174,11 @@ ALTER TABLE `ban`
   MODIFY COLUMN `unbanned_ip` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   ADD INDEX `computerid` (`computerid`),
   ADD INDEX `ip` (`ip`);
+
+ALTER TABLE `ban` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+/* General conversion */
+ALTER TABLE `erro_connection_log` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `erro_player` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `library` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `whitelist` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
