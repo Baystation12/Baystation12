c4a5e9d8c92252fbdcef18cac0d9e5572cc57b85
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-06-16 02:59:33 +0300

    `$HOME` -> `~`

--- install-byond.sh
+++ install-byond.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
-if [ -f "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker" ];
+if [ -f ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker ];
 then
   echo "Using cached directory."
 else
   echo "Setting up BYOND."
-  mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
-  cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
+  mkdir -p ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
+  cd ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
   echo "Installing DreamMaker to $PWD"
   curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip -o byond.zip
