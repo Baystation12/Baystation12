6fda9f7766c158afa0c1066f19e83e4efdee3245
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-11-25 12:03:22 +0300

    Bump RustG version to 3.0.0

--- scripts/install-rust_g-dependencies.sh
+++ scripts/install-rust_g-dependencies.sh
@@ -3,8 +3,4 @@ set -euo pipefail
 
 sudo dpkg --add-architecture i386
 sudo apt update || true
-sudo apt remove -y libssl1.1:amd64 || true
 sudo apt install libgcc-s1:i386
-sudo apt install -o APT::Immediate-Configure=false libc6:i386
-wget http://ftp.de.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.1_1.1.1n-0+deb10u6_i386.deb
-sudo dpkg -i libssl1.1_1.1.1n-0+deb10u6_i386.deb
