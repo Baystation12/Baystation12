0e9dfed23aaf37e17e24e0256881fee504fa8302
Gregory Maddra <bloxgate@gmail.com>
2025-11-02 22:13:21 -0600

    See if switching to HTTPS fixes downloads

--- install-byond.sh
+++ install-byond.sh
@@ -8,7 +8,7 @@ else
   mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
   cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
   echo "Installing DreamMaker to $PWD"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl "https://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip -o byond.zip
   cd byond
   make here
