d0a586c98def50c7d421187f2ae4fb9ef2748bf6
HeyBanditoz <banditoz@protonmail.com>
2025-12-27 02:33:29 -0700

    Fix CL author gen with trailing space after cl and empty PR descs

--- tools/changelog/generate_cl.py
+++ tools/changelog/generate_cl.py
@@ -28,7 +28,7 @@ from pathlib import Path
 from ruamel import yaml
 from github import Github, Auth, InputGitAuthor
 
-CL_BODY = re.compile(r"(:cl:|🆑)(.+)?\r\n((.|\n|\r)+?)\r\n\/(:cl:|🆑)", re.MULTILINE)
+CL_BODY = re.compile(r"(:cl:|🆑)\s*(.*)?\r\n((.|\n|\r)+?)\r\n\/(:cl:|🆑)", re.MULTILINE)
 CL_SPLIT = re.compile(r"(^\w+):\s+(\w.+)", re.MULTILINE)
 
 git_email = os.getenv("GIT_EMAIL")
@@ -63,8 +63,11 @@ except AttributeError:
     print("No CL found!")
     exit(0) # Change to '0' if you do not want the action to fail when no CL is provided
 
+if not cl or not cl_list:
+    print("No CL found!")
+    exit(0)
 
-if cl.group(2) is not None:
+if cl.group(2):
     write_cl['author'] = cl.group(2).lstrip()
 else:
     write_cl['author'] = pr_author
